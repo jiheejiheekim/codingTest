36d0ce6dca4d60b7a5a32b5998c455c1a1b1dabf
jiheejiheekim <jihee2518@gmail.com>
2024-07-08 14:37:45 +0900

    10번 각도기 ~ 13번 중복된 숫자 개수

--- src/level_0/lv0_1_to_20.java
+++ src/level_0/lv0_1_to_20.java
@@ -1,5 +1,7 @@
 package level_0;
 
+import java.util.Arrays;
+
 public class lv0_1_to_20 {
 	
 	static class Solution{
@@ -40,16 +42,64 @@ public class lv0_1_to_20 {
 			각 angle이 매개변수로 주어질 때 예각일 때 1, 직각일 때 2, 둔각일 때 3, 평각일 때 4를 return하도록 solution 함수를 완성해주세요.
 		*/
 		public int solution10(int angle) {
-	        int answer = 0;
+			int answer = 0;
+	        if(angle>0 && angle<90) {
+	            answer=1;
+	        }else if(angle==90){
+	            answer=2;
+	        }else if(angle>90 && angle<180){
+	            answer=3;
+	        }else if(angle==180){
+	            answer=4;
+	        }
 	        return answer;
 	    }
 		
+		/*
+			11) 짝수의 합
+			정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+		*/
+		public int solution11(int n) {
+			 int answer = 0;
+		        if(n>0 && n<=1000){
+		            for(int i=1; i<=n; i++){
+		                if(i%2==0){
+		                    answer+=i;
+		                }
+		            }
+		        }
+		        return answer;
+	    }
 		
+		/*
+			12) 배열 뒤집기
+			정수가 들어 있는 배열 num_list가 매개변수로 주어집니다. 
+			num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
+		*/
+		public int[] solution12(int[] num_list) {
+			int[] answer = {};
+	        int len = num_list.length;
+	        answer = new int[len];
+	        for(int i=0; i<len; i++){
+	            answer[i] = num_list[len-i-1];
+	        }
+	        return answer;
+	    }
 		
-		
-		
-		
-		
+		/*
+			13) 중복된 숫자 개수
+			정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때, 
+			array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
+		*/
+		public int solution13(int[] array, int n) {
+			int answer = 0;
+	        for(int i=0; i<array.length; i++){
+	            if(array[i]==n){
+	                answer++;
+	            }
+	        }
+	        return answer;
+	    }
 		
 	}
 	
@@ -68,9 +118,37 @@ public class lv0_1_to_20 {
        	System.out.println("9번 문제 : "+answer9);
        	
        	//10
+       	int result10 = sol.solution10(95);
+       	System.out.println("10번 문제 : "+result10);
        	
        	//11
+       	int result11 = sol.solution11(18);
+       	System.out.println("11번 문제 : "+result11);
+       	
+       	//12
+       	int[] num_list = {1, 2, 3, 4, 5};
+       	
+       	int[] answer = {};
+        int len = num_list.length;
+        answer = new int[len];
+        //System.out.println(num_list[len-1]);
+        for(int i=0; i<len; i++){
+            answer[i] = num_list[len-i-1];
+        }
+        System.out.print("12번 문제 : "+Arrays.toString(answer));
+	
+		//13
+        int[] array = {1, 2, 1, 4, 1};
+        int n13 = 1;
+        int result13 = sol.solution13(array, n13);
+        System.out.println("\n13번 문제 : "+result13);
+        
+		//14
 		
+		
+		//15
+        
+        
+        
 	}
-
 }
